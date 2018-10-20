e1055ef1e610ceb8aae618f9cb50a697a807915d
Sebastian <sebastian.anonim97@gmail.com>
2018-10-20 20:22:26 +0200

    added zad02 & fix err

--- zad01/src/zad01/HelloWorld.java
+++ zad01/src/zad01/HelloWorld.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:zad01/src/zad01/HelloWorld.java
 package zad01;
 
 public class HelloWorld {
@@ -6,3 +7,10 @@ public class HelloWorld {
     }
 }
 
+=======
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+>>>>>>> parent of e888cc6... fix:zad01/src/HelloWorld.java
